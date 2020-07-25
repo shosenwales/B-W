eeb299da7448a0797b3d5a73c7b723adae56a8cb
shuaib <wales149@gmail.com>
2020-07-25 16:55:50 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function openSlideMenu(){
+   document.getElementById('slide-menu').style.width='250px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('slide-menu').style.width = '0';
+}
